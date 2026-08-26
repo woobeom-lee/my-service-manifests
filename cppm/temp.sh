d85dc2c13ced2072e8e4f0a02b1c395a8f9f8288
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 08:40:16 +0900

    fix: remove clair-db postStartHook to prevent kubelet timeout killing

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,10 +1,9 @@
 cd /home/registry/my-service-manifests
  
-# 1. clair-db 복원 스크립트 끝에 '|| true' 방패를 장착합니다.
-sed -i 's/clair2x-vuln-db.sql -c/clair2x-vuln-db.sql -c || true/g' cppm/templates/1-database/*clair-db*.yaml
-sed -i 's/|| true || true/|| true/g' cppm/templates/1-database/*clair-db*.yaml
+# 1. clair-db 설계도에서 말썽을 일으키는 lifecycle 블록을 통째로 삭제합니다.
+sed -i '/lifecycle:/,/clair2x-vuln-db.sql/d' cppm/templates/1-database/*clair-db*.yaml
  
 # 2. 깃허브로 전송!
 git add .
-git commit -m "fix: append || true to clair-db postStartHook to ignore restore warnings"
+git commit -m "fix: remove clair-db postStartHook to prevent kubelet timeout killing"
 git push origin main
