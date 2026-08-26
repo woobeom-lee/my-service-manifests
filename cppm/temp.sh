d2cb01fb89fc365a09c447f8ce20e56ff330ddc8
Woobeom Lee <lwb74@ahnlab.com>
2026-08-26 15:16:53 +0900

    fix: precisely remove wait-for-dependencies without breaking init-link

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,12 +1,20 @@
-cd /home/registry/my-service-manifests/cppm/templates/
+# 1. 가장 최근에 제가 드린 '망가진 커밋'을 안전하게 롤백(Revert) 하여 코드를 원상 복구합니다.
+cd /home/registry/my-service-manifests
+git revert HEAD --no-edit
  
-# 1. 임무가 끝난 정찰병(initContainers) 코드를 3-core, 4-web 전체 파일에서 싹 삭제합니다.
-find 3-core 4-web -type f -name "*.yaml" -exec sed -i '/initContainers:/d' {} \;
-find 3-core 4-web -type f -name "*.yaml" -exec sed -i '/cppm.wait-for-dependencies/d' {} \;
+# 2. 정밀 타격! 진짜 '정찰병(wait-for-dependencies)' 코드만 삭제하고, 
+# 껍데기만 남은 중복 initContainers 줄을 안전하게 지워줍니다.
+find cppm/templates -type f -name "*.yaml" -exec sed -i '/cppm.wait-for-dependencies/d' {} \;
+find cppm/templates -type f -name "*.yaml" -exec sed -i -z 's/[ \t]*initContainers:[ \t]*\n[ \t]*containers:/\n      containers:/g' {} \;
  
-# 2. 옛날 버릇 못 고친 rsyslog 파일에 Root 권한(VIP)을 부여합니다.
-find . -type f -name "*rsyslog*.yaml" | while read file; do
+# 3. 아까 롤백되면서 날아갔던 rsyslog의 루트 권한(VIP)만 다시 살포시 얹어줍니다.
+find cppm/templates -type f -name "*rsyslog*.yaml" | while read file; do
     if ! grep -q "runAsUser: 0" "$file"; then
         sed -i '/image:/i \          securityContext:\n            runAsUser: 0' "$file"
     fi
 done
+ 
+# 4. 깃허브에 다시 푸시합니다!
+git add .
+git commit -m "fix: precisely remove wait-for-dependencies without breaking init-link"
+git push origin main
