f328dbade54a09c1546f88125c18627e44422d6a
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 11:29:48 +0900

    fix: add missing cpp-mongo-mongos to hostAliases for all apps

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,11 +1,9 @@
-cd /home/registry/my-service-manifests
+d /home/registry/my-service-manifests
  
-# 1. 뒤에서 일하는 4명의 핵심 일꾼들에게 마스터 나침반을 모두 주입합니다.
-for app in "epp-batch-processor" "epp-scheduler" "epp-tomcat-agent" "epp-tomcat-auth"; do
-    find cppm/templates -type f -name "*${app}*.yaml" -exec sed -i '/- "host.docker.internal"/a \            - "cpp-mongo"\n            - "epp-mongo-mongos"\n            - "cpp-pgbouncer-eppoltp"\n            - "epp-pgbouncer-eppoltp"\n            - "cpp-redis"\n            - "epp-redis"\n            - "cpp-kafka"\n            - "epp-kafka"' {} \;
-done
+# 1. 아까 추가했던 몽고DB 이름표 바로 밑에, 앱이 진짜로 찾는 이름(cpp-mongo-mongos)을 일괄 추가합니다!
+find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-mongo-mongos"/a \            - "cpp-mongo-mongos"' {} \;
  
 # 2. 깃허브로 즉시 전송!
 git add .
-git commit -m "fix: inject all master hostAliases to agent and batch pods"
+git commit -m "fix: add missing cpp-mongo-mongos to hostAliases for all apps"
 git push origin main
