b0a43808b455099dc181657c126a50b959b30204
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 13:30:47 +0900

    fix: apply ClusterIP and accurate ports to all cpp alias services

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,7 +1,9 @@
-kubectl get pod -n k8s-cppm | grep epp-rsyslog | awk '{print $1}' | while read pod
-do
-  kubectl delete pod "$pod" \
-    -n k8s-cppm \
-    --grace-period=0 \
-    --force
-done
+cd /home/registry/my-service-manifests
+ 
+# 1. 현재 수정된 파일 상태 확인 (빨간 글씨로 수정된 파일들이 보여야 정상입니다)
+git status
+ 
+# 2. 수정한 파일들을 모두 장바구니에 담고 깃허브로 강력하게 쏴줍니다!
+git add .
+git commit -m "fix: apply ClusterIP and accurate ports to all cpp alias services"
+git push origin main
