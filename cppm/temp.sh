6ab8005686603e150630721eff6dcf2939ecce87
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 11:19:56 +0900

    fix: inject all master hostAliases to agent and batch pods

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,9 +1,11 @@
 cd /home/registry/my-service-manifests
  
-# 1. tomcat-console 파일이 어느 폴더에 있든 자동으로 찾아서 나침반 명단을 꽂아 넣습니다.
-find cppm/templates -type f -name "*tomcat-console*.yaml" -exec sed -i '/- "host.docker.internal"/a \            - "cpp-mongo"\n            - "epp-mongo-mongos"\n            - "cpp-pgbouncer-eppoltp"\n            - "epp-pgbouncer-eppoltp"\n            - "cpp-redis"\n            - "epp-redis"' {} \;
+# 1. 뒤에서 일하는 4명의 핵심 일꾼들에게 마스터 나침반을 모두 주입합니다.
+for app in "epp-batch-processor" "epp-scheduler" "epp-tomcat-agent" "epp-tomcat-auth"; do
+    find cppm/templates -type f -name "*${app}*.yaml" -exec sed -i '/- "host.docker.internal"/a \            - "cpp-mongo"\n            - "epp-mongo-mongos"\n            - "cpp-pgbouncer-eppoltp"\n            - "epp-pgbouncer-eppoltp"\n            - "cpp-redis"\n            - "epp-redis"\n            - "cpp-kafka"\n            - "epp-kafka"' {} \;
+done
  
-# 2. 깃허브로 다시 전송!
+# 2. 깃허브로 즉시 전송!
 git add .
-git commit -m "fix: inject master hostAliases to tomcat-console using find"
+git commit -m "fix: inject all master hostAliases to agent and batch pods"
 git push origin main
