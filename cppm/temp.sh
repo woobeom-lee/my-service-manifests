407c1bfe0b3419dc9808a8568f0b721d5b7d8f95
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 13:22:48 +0900

    fix: convert alias services to..

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,14 +1,7 @@
-cd /home/registry/my-service-manifests
- 
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-mongo/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-mongo/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-pgbouncer/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-pgbouncer/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-redis/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-redis/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-kafka/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-kafka/d' {} \;
- 
-git add .
-git commit -m "fix: remove bad hostAliases preventing internal K8s DNS resolution"
-git push origin main
+kubectl get pod -n k8s-cppm | grep epp-rsyslog | awk '{print $1}' | while read pod
+do
+  kubectl delete pod "$pod" \
+    -n k8s-cppm \
+    --grace-period=0 \
+    --force
+done
