b18b028672ea6cb6b13a03eebabc5739b0540034
Woobeom Lee <lwb74@ahnlab.com>
2026-08-27 12:42:34 +0900

    fix: remove bad hostAliases and convert ExternalName services to ClusterIP A-records

--- cppm/temp.sh
+++ cppm/temp.sh
@@ -1,14 +1,15 @@
 cd /home/registry/my-service-manifests
  
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-mongo/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-mongo/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-pgbouncer/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-pgbouncer/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-redis/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-redis/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "cpp-kafka/d' {} \;
-find cppm/templates -type f -name "*.yaml" -exec sed -i '/- "epp-kafka/d' {} \;
+# 1. 제가 주입했던 엉터리 hostAliases 찌꺼기 완벽 청소 (원상 복구)
+for host in cpp-mongo cpp-mongo-mongos epp-mongo-mongos cpp-pgbouncer-eppoltp epp-pgbouncer-eppoltp cpp-redis epp-redis cpp-kafka epp-kafka; do
+    find cppm/templates -type f -name "*.yaml" -exec sed -i "/- \"$host\"/d" {} \;
+done
  
+# 2. [핵심] 기존의 ExternalName 서비스를 ClusterIP(A-Record)로 영구 변환!
+find cppm/templates -type f -name "*.yaml" -exec sed -i 's/type: ExternalName/type: ClusterIP/g' {} \;
+find cppm/templates -type f -name "*.yaml" -exec sed -i 's/^[[:space:]]*externalName:[[:space:]]*"\?epp-\([^"]*\)"\?/  selector:\n    app: epp-\1/g' {} \;
+ 
+# 3. 깃허브로 즉시 전송!
 git add .
-git commit -m "fix: remove bad hostAliases preventing internal K8s DNS resolution"
+git commit -m "fix: remove bad hostAliases and convert ExternalName services to ClusterIP A-records"
 git push origin main
